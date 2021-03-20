26beffa8a0c2cea013974df20f91d3facd342993
Link <50463727+LINKdiscordd@users.noreply.github.com>
2021-03-20 03:53:16 +0100

    Create example.js

---
+++ examples/example.js
@@ -0,0 +1,33 @@
+const mineflayer = require('mineflayer')
+const { autoCrystal } = require('mineflayer-autocrystal')
+
+const bot = mineflayer.createBot({
+	host: 'localhost',
+	username: 'AutoCrystal',
+})
+
+bot.loadPlugin(autoCrystal)
+
+bot.once('spawn', () => {
+    bot.chat('/gamemode creative')
+    bot.chat('/give @s end_crystal 500')
+})
+
+bot.on('message', async (username, message) => {
+	if (username === bot.username) return
+
+	if (message === 'start') {
+		console.log('AutoCrystal enabled.')
+		bot.autoCrystal.enable()
+	}
+
+	else if (message === 'stop') {
+		console.log('AutoCrystal disabled.')
+		bot.autoCrystal.disable()
+	}
+
+	else if (message === 'holes') {
+		const holes = await bot.autoCrystal.getHoles()
+		console.log(`Found ${holes.length} holes made out of bedrock.`)
+	}
+})
