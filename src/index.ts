b5d5eec53c22a111aadd24254e601d3544896204
Link <50463727+link-discord@users.noreply.github.com>
2021-08-16 17:03:21 +0200

    include docs files only

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { AutoCrystal } from './AutoCrystal'
-import { Bot } from 'mineflayer'
-
-export function autoCrystal(bot: Bot) {
-    // @ts-ignore
-    bot.autoCrystal = new AutoCrystal(bot)
-}
-
-export { AutoCrystal } from './AutoCrystal'
